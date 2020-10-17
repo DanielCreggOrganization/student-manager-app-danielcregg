b10404cb19a9f6fb4d9bdf82f3a5f0649e637b70
Daniel Cregg <daniel.cregg@gmit.ie>
2020-10-17 13:21:31 +0100

    Updated Student Class

--- src/ie/gmit/studentmanagerpackage/Student.java
+++ src/ie/gmit/studentmanagerpackage/Student.java
@@ -1,5 +1,18 @@
 package ie.gmit.studentmanagerpackage;
 
 public class Student {
+	
+	//Instance Variables
+	private String studentId;
 
+	// Getters
+	public String getStudentId() {
+		return studentId;
+	}
+	
+	// Setters
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	
 }
